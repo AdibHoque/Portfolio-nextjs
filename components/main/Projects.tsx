5b7a6f5546ae52192389656e45895014b5bfe9a6
AdibHoque <adibhoque04@gmail.com>
2024-07-04 15:30:26 +0600

    Little Fix

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -10,7 +10,7 @@ const Projects = () => {
       <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
         My Projects
       </h1>
-      <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10">
+      <div className="h-full w-full flex flex-col md:flex-row gap-10 px-4 md:px-10">
         <ProjectCard
           src="/Truebond-Image.png"
           title="Truebond Matrimony"
@@ -18,7 +18,7 @@ const Projects = () => {
           link="https://truebond-matrimony.web.app/"
         />
         <ProjectCard
-          src="/Jobseekers-Image.png"
+          src="/JobSeekers-Image.png"
           title="JobSeekers BD"
           description="JobSeekers BD is an online job portal designed to connect job seekers with potential employers. The platform offers a range of features to facilitate job searching, application management, and job posting."
           link="https://jobseekers-bd.web.app/"
